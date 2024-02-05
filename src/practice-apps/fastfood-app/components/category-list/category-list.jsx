a7103b8fa1810544820316a314877b4aa00e2a07
Alirezaw <Alirezawmoradi@gmail.com>
2024-02-05 14:23:25 +0330

    loading component created and added to category list component

--- src/practice-apps/fastfood-app/components/category-list/category-list.jsx
+++ src/practice-apps/fastfood-app/components/category-list/category-list.jsx
@@ -1,14 +1,17 @@
 import {useEffect, useState} from "react";
 import axios from "../../../../core/axios.js";
+import Loading from "../loading/loading.jsx";
 
 const CategoryList = () => {
 
+    const [loading, setLoading] = useState(true);
     const [categories, setCategories] = useState([]);
 
     useEffect(() => {
         const fetchCategories = async () => {
             const response = await axios.get('/FoodCategory/categories');
             setCategories(response.data);
+            setLoading(false);
         }
         fetchCategories();
     }, []);
@@ -16,22 +19,26 @@ const CategoryList = () => {
     return (
         <nav className='container mt-n5'>
             <div className='d-flex align-items-center bg-white rounded-3 shadow-lg py-4' style={{height: '80px'}}>
-                <ul className='nav'>
-                    <li className='nav-item'>
-                        <a className='nav-link' href='#'>
-                            همه فست فودها
-                        </a>
-                    </li>
-                    {
-                        categories.map((categories) => (
-                            <li className='nav-item' key={categories.id}>
+                {
+                    loading ? <Loading theme='primary'/>
+                        :
+                        <ul className='nav'>
+                            <li className='nav-item'>
                                 <a className='nav-link' href='#'>
-                                    {categories.name}
+                                    همه فست فودها
                                 </a>
                             </li>
-                        ))
-                    }
-                </ul>
+                            {
+                                categories.map((categories) => (
+                                    <li className='nav-item' key={categories.id}>
+                                        <a className='nav-link' href='#'>
+                                            {categories.name}
+                                        </a>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                }
             </div>
         </nav>
     )
--- src/practice-apps/fastfood-app/components/header/header.jsx
+++ src/practice-apps/fastfood-app/components/header/header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <header className='bg-size-cover bg-position-center py-5'>
             <div className='container'>
-                <h1 className='text-light text-center text-uppercase'>
+                <h1 className='text-light text-start text-uppercase'>
                     FastFood App
                 </h1>
             </div>
