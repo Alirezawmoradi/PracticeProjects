22e322eb595f699c0e3e8c74e4df5d0900155420
Alirezaw <Alirezawmoradi@gmail.com>
2024-02-05 19:52:36 +0330

    searchbar component added

--- src/practice-apps/fastfood-app/components/category-list/category-list.jsx
+++ src/practice-apps/fastfood-app/components/category-list/category-list.jsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import axios from "../../../../core/axios.js";
 import Loading from "../loading/loading.jsx";
+import Searchbar from "../searchbar/searchbar.jsx";
 
 const CategoryList = ({filterItems}) => {
 
@@ -22,23 +23,26 @@ const CategoryList = ({filterItems}) => {
                 {
                     loading ? <Loading theme='primary'/>
                         :
-                        <ul className='nav'>
-                            <li className='nav-item' onClick={() => filterItems()}>
-                                <a className='nav-link' href='#'>
-                                    همه فست فودها
-                                </a>
-                            </li>
-                            {
-                                categories.map((category) => (
-                                    <li className='nav-item' key={category.id}
-                                        onClick={() => filterItems(category.id)}>
-                                        <a className='nav-link' href='#'>
-                                            {category.name}
-                                        </a>
-                                    </li>
-                                ))
-                            }
-                        </ul>
+                        <div className='ps-3 w-100 d-flex align-items-center justify-content-between gap-5'>
+                            <ul className='nav'>
+                                <li className='nav-item' onClick={() => filterItems()}>
+                                    <a className='nav-link' href='#'>
+                                        همه فست فودها
+                                    </a>
+                                </li>
+                                {
+                                    categories.map((category) => (
+                                        <li className='nav-item' key={category.id}
+                                            onClick={() => filterItems(category.id)}>
+                                            <a className='nav-link' href='#'>
+                                                {category.name}
+                                            </a>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                            <Searchbar/>
+                        </div>
                 }
             </div>
         </nav>
---
+++ src/practice-apps/fastfood-app/components/searchbar/searchbar.jsx
@@ -0,0 +1,27 @@
+import {BsSearch} from "react-icons/bs";
+import {useState} from "react";
+
+const SearchBar = () => {
+    const [value, setValue] = useState('');
+    const handleValue = (e) => {
+        setValue(e.target.value)
+    }
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(value)
+    }
+    return (
+        <form onSubmit={onSubmit} className='search flex-fill d-flex align-items-center'>
+            <div className='input-group'>
+                <input className='form-control rounded-end pe-5 border-success'
+                       type='text'
+                       placeholder='جستجوی فست فود ...'
+                       value={value}
+                       onChange={handleValue}
+                />
+                <BsSearch className='position-absolute top-50 translate-middle-y text-muted me-3'/>
+            </div>
+        </form>
+    )
+}
+export default SearchBar;
