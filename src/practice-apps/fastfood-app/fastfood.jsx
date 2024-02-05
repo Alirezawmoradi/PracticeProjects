947ad54d7aa177bbd333fa059d5f1b4ec2ac3c89
Alirezaw <Alirezawmoradi@gmail.com>
2024-02-05 13:43:00 +0330

    axios added to project data fetched in category list component

--- src/practice-apps/fastfood-app/fastfood.jsx
+++ src/practice-apps/fastfood-app/fastfood.jsx
@@ -1,10 +1,11 @@
 import Header from "./components/header/header.jsx";
+import CategoryList from "./components/category-list/category-list.jsx";
 
 const FastFood = () => {
     return (
         <div className='wrapper bg-faded-dark'>
             <Header/>
-            FastFood
+            <CategoryList/>
         </div>
     )
 }
