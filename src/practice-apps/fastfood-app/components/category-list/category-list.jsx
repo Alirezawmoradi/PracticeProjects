2e98dc87fedf9ec0b05ec5b52fa8d70d40b68eee
Alirezaw <Alirezawmoradi@gmail.com>
2024-02-05 14:09:42 +0330

    navbar items added to nav bar using fetching data

--- src/practice-apps/fastfood-app/components/category-list/category-list.jsx
+++ src/practice-apps/fastfood-app/components/category-list/category-list.jsx
@@ -1,18 +1,39 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import axios from "../../../../core/axios.js";
 
 const CategoryList = () => {
 
+    const [categories, setCategories] = useState([]);
+
     useEffect(() => {
         const fetchCategories = async () => {
             const response = await axios.get('/FoodCategory/categories');
-            console.log(response.data);
+            setCategories(response.data);
         }
         fetchCategories();
     }, []);
 
     return (
-        <div>Category List</div>
+        <nav className='container mt-n5'>
+            <div className='d-flex align-items-center bg-white rounded-3 shadow-lg py-4' style={{height: '80px'}}>
+                <ul className='nav'>
+                    <li className='nav-item'>
+                        <a className='nav-link' href='#'>
+                            همه فست فودها
+                        </a>
+                    </li>
+                    {
+                        categories.map((categories) => (
+                            <li className='nav-item' key={categories.id}>
+                                <a className='nav-link' href='#'>
+                                    {categories.name}
+                                </a>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+        </nav>
     )
 }
 export default CategoryList;
