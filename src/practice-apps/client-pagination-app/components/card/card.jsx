b494d1b46f59a1ebc2f9351c3de5a19429ca4ebb
Alirezaw <Alirezawmoradi@gmail.com>
2024-02-07 17:52:30 +0330

    pagination component added client side pagination finished

--- src/practice-apps/client-pagination-app/components/card/card.jsx
+++ src/practice-apps/client-pagination-app/components/card/card.jsx
@@ -4,7 +4,7 @@ const Card = ({imageUrl, name, skills, mobile, email}) => {
     return (
         <div className='card text-center border-0 shadow-lg'>
             <div className='card-body'>
-                <img className='d-inline-block rounded-circle mb-3' src={imageUrl} width='96'/>
+                <img className='d-inline-block rounded-circle mb-3' src={imageUrl} width='96' alt=''/>
                 <h6>{name}</h6>
                 <p className='fs-sm text-muted'>{skills}</p>
                 <p className='mt-4 mb-0 fs-sm fw-bold'>{mobile}</p>
