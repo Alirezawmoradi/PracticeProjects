103b0b10a2a96f685e00bdb4b9b54728ffc0aee9
Alirezaw <Alirezawmoradi@gmail.com>
2024-02-07 16:59:43 +0330

    card component created and programmers data set to page

--- src/practice-apps/client-pagination-app/client-pagination-app.jsx
+++ src/practice-apps/client-pagination-app/client-pagination-app.jsx
@@ -1,11 +1,39 @@
 import usePaginatedFetch from "./hooks/usePaginatedFetch.jsx";
+import {useEffect, useState} from "react";
+import Card from "./components/card/card.jsx";
 
 const url = 'https://react-mini-projects-api.classbon.com/Programmer/programmers';
 const ClientPaginationApp = () => {
-    const [loading, programmers] = usePaginatedFetch(url,3)
+    const [loading, data] = usePaginatedFetch(url, 3);
+    const [page, setPage] = useState(1);
+    const [programmers, setProgrammers] = useState([]);
+
+    useEffect(() => {
+        if (loading) return;
+        setProgrammers(data[page - 1]);
+    }, [loading]);
+
     return (
-        <div>
-            Client Pagination App
+        <div className='container pt-5'>
+            {
+                loading ? (
+                    <div className='d-flex justify-content-center'>
+                        <div className='spinner-border'></div>
+                    </div>
+                ) : (
+                    <div className='row d-flex justify-content-center'>
+                        {
+                            programmers.map(({id, ...programmer}) => {
+                                return (
+                                    <div className='col-3' key={id}>
+                                        <Card {...programmer}/>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                )
+            }
         </div>
     )
 }
