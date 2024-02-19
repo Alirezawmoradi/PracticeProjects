ff56a91a820703accf0dd3380455c25af91539e7
Alirezaw <Alirezawmoradi@gmail.com>
2024-02-19 21:10:01 +0330

    change badge number created

--- src/practice-apps/cart-introduction/components/navbar/navbar.jsx
+++ src/practice-apps/cart-introduction/components/navbar/navbar.jsx
@@ -1,12 +1,14 @@
 import {RiShoppingCartLine} from "react-icons/ri";
+import {useCartContext} from "../../context/context.jsx";
 
 export const Navbar = () => {
+    const {foodItems} = useCartContext();
     return (
         <div className='navbar navbar-dark bg-dark shadow-sm'>
             <div className='container'>
                 <a className='navbar-tool ms-3'>
                     <div className='navbar-tool-icon-box bg-secondary'>
-                        <span className='navbar-tool-label d-flex justify-content-center'>4</span>
+                        <span className='navbar-tool-label d-flex justify-content-center'>{foodItems.length}</span>
                         <RiShoppingCartLine className='navbar-tool-icon'/>
                     </div>
                 </a>
