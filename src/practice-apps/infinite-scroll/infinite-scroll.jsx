b6ff8e77f226a3a05fb5d14f71659899844d583d
Alirezaw <Alirezawmoradi@gmail.com>
2024-02-20 19:15:33 +0330

    useCommentsData custom hook added to infinite scroll page

--- src/practice-apps/infinite-scroll/infinite-scroll.jsx
+++ src/practice-apps/infinite-scroll/infinite-scroll.jsx
@@ -1,7 +1,31 @@
 import {HeroSection} from "./compnents/hero-section.jsx";
+import useCommentsData from "./hooks/useCommentsData.jsx";
+import {Comments} from "./compnents/comments.jsx";
 
 export const InfiniteScroll = () => {
+    const {comments, loading,} = useCommentsData({
+        url: '/Comments/1'
+    })
     return (
-        <HeroSection/>
+        <div className='container pt-5'>
+            <HeroSection/>
+            <div className='row'>
+                <div className='col-12 pt-5'>
+                    {
+                        loading ?
+                            (
+                                <div className='d-flex justify-content-center'>
+                                    <div className='spinner-border'></div>
+                                </div>
+                            )
+                            :
+                            comments.map((comment) => {
+                                return <Comments key={comment.id} {...comment}/>
+                            })
+                    }
+
+                </div>
+            </div>
+        </div>
     )
 }
