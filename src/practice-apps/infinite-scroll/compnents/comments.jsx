f0b0d6f7abe06b49cef95f541989570a39c5f381
Alirezaw <Alirezawmoradi@gmail.com>
2024-02-20 19:15:54 +0330

    comments component created and data fetched

---
+++ src/practice-apps/infinite-scroll/compnents/comments.jsx
@@ -0,0 +1,18 @@
+export const Comments = ({name, imageUrl, description, date}) => {
+    return (
+        <div className='d-flex align-items-start py-3 border-bottom'>
+            <img className='rounded-circle' width='50' src={imageUrl}/>
+            <div className='pe-3'>
+                <h6 className='mb-0'>
+                    {name}
+                </h6>
+                <p className='mb-1 mt-1 fs-sm'>
+                    {description}
+                </p>
+                <span className='fs-ms text-muted'>
+                    {date}
+                </span>
+            </div>
+        </div>
+    )
+}
